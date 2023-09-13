f8f339d2f9512a05516b9aeb9a3b501e5e22bbbd
Ankur Shashwat <ankurshashwhat>
2023-09-13 23:23:36 +0530

    chore: init Next.js 13 project

---
+++ client/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["lh3.googleusercontent.com", "www.flaticon.com"],
+  },
+};
+
+module.exports = nextConfig;
